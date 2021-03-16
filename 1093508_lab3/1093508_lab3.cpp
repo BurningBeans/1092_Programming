62910874a961b306ada5b2dd627a3cbd5fbe2d5f
BurningBeans <syy102677@gmail.com>
2021-03-16 19:11:22 +0800

    lab3

--- 1093508_lab3/1093508_lab3.cpp
+++ 1093508_lab3/1093508_lab3.cpp
@@ -1,36 +1,99 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include "stack.h"
 using namespace std;
+class Stack{
+    private:
+    int stackSize;
+    int stackPtr;
+    char *c_array;
+    public:
+    Stack(int = 1, int = 0, char * = NULL);
+    void push(char);
+    char pop();
+    void reset();
+    bool empty();
+    bool full();
+    //void print(); // debug function
+};
+Stack::Stack(int size, int ptr,char *c_arr)
+{
+    stackSize = size;
+    stackPtr = ptr;
+    c_array = c_arr;
+}
+void Stack::push(char c)
+{
+    c_array[stackPtr] = c;
+    stackPtr++;
+}
+char Stack::pop()
+{
+    char c = c_array[stackPtr];
+    c_array[stackPtr] = NULL;
+    stackPtr--;
+    return c;
+}
+void Stack::reset()
+{
+    stackPtr = 0;
+    //cout <<"The stack is reset.\n";
+}
+bool Stack::empty()
+{
+    if(stackPtr == 0)
+        return true;
+    return false;
+}
+bool Stack::full()
+{
+    if(stackPtr == stackSize)
+        return true;
+    return false;
+}
+/*
+void Stack::print()
+{
+    cout << c_array;
+    cout << endl;
+}
+*/
 void checkExpression(string, Stack &);
 int main()
 {
-    int stacksize = 0;//
+    int stacksize = 0;
     cin >> stacksize;
-    stacksize;
     int inputs = 0;
     cin >> inputs;
-    Stack aStack(stacksize,0,NULL);
+    Stack aStack(stacksize, 0, new char[stacksize]);
     string str = "";
     for(int i = 0; i < inputs; i++)
     {
         cin >> str;
+        cout << "\n";
         checkExpression(str,aStack);
+        cout << '\n';
     }
 }
 void checkExpression(string str, Stack &aStack)
 {
+    if(str.empty())//Not used in the example output
+    {
+        cout << "Empty input!";
+        return;
+    }
     for(char i: str)
     {
-        if(aStack.full())
-        {
-            cout << "Stack is full" << endl;
-            aStack.reset();
-            return;
-        }
         if(i == '(')
-            aStack.push(i);
+        {
+            if(aStack.full())
+            {
+                cout << "Stack is full" << endl;
+                aStack.reset();
+                return;
+            }
+            else
+                aStack.push(i);
+        }
         if(i == ')')
         {
             if(aStack.empty())
@@ -54,36 +117,4 @@ void checkExpression(string str, Stack &aStack)
         cout <<"correct\n";
     }
     aStack.reset();
-    /*
-    int counters = 0;//if counter < 0 meaning "(" is more than ")"
-    while(!aStack.empty())
-    {
-        if(0 > counters)
-        {
-            break;
-        }
-        if(aStack.pop()==')')
-        {
-            counters++;
-        }
-        else if(aStack.pop()=='(')
-        {
-            counters--;
-        }
-    }
-    //
-    if(counters > 0)
-    {
-        cout << "more )'s \n";
-    }
-    else if(counters < 0)
-    {
-        cout << "more ('s \n";
-    }
-    else
-    {
-        cout << "correct \n";
-    }
-    aStack.reset();
-    */
 }
