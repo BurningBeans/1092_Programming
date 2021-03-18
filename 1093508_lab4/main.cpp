fa9c9b65d98ebe1ba9d786092482398c7442e406
BurningBeans <syy102677@gmail.com>
2021-03-18 10:48:22 +0800

    lab4

---
+++ 1093508_lab4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "queue.h"
+#include "queue.cpp"
+using namespace std;
+int main()
+{
+    int size = 0;
+    cin >> size;
+    Queue idealQueue = Queue(size);
+    Queue waitingQueue = Queue(size);
+
+}
---
+++ 1093508_lab4/queue.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "queue.h"
+using namespace std;
+Queue::Queue(int insize)
+{
+    cout << "A Queue of size " << insize << " is created." << endl;
+    size = insize;
+    intQueue = new int [size];
+    front = 0;
+    rear = 0;
+    count = 0;
+}
+Queue::~Queue()
+{
+    cout <<"A Queue of size " << size << " is destruct." <<endl;
+}
+void Queue::enqueue(int num)
+{
+    intQueue[rear] = num;
+    rear++;
+    count++;
+}
+int Queue::dequeue()
+{
+    int returning = intQueue[front];
+    for(int i = 0; i < count-1; i++)
+    {
+        intQueue[i] = intQueue[i+1];
+    }
+    rear--;
+    count--;
+    return returning;
+}
+int Queue::peek()
+{
+    return intQueue[front];
+}
+int Queue::getCount()
+{
+    return count;
+}
+int Queue::getSize()
+{
+    return size;
+}
+bool Queue::isEmpty()
+{
+    if(count == 0)
+        return true;
+    return false;
+}
+bool Queue::isFull()
+{
+    if(count == size)
+        return true;
+    return false;
+}
+void Queue::clearQueue()
+{
+    delete []intQueue;
+    intQueue = new int [size];
+    front = 0;
+    rear = 0;
+    count = 0;
+}
+void Queue::printQueue()
+{
+    for(int i = 0 ; i < count ; i++)
+    {
+        cout << intQueue[i] << " \n";
+    }
+}
